1e8060a912602d5b1662b96bb28ceab3641bc8fc
Wesley Tokoi <tokoiwesley@gmail.com>
2021-08-15 22:05:50 +0300

    Create and run a Kotlin app

--- app/src/main/java/dev/tokoi/notekeeperkt/MainActivity.kt
+++ app/src/main/java/dev/tokoi/notekeeperkt/MainActivity.kt
@@ -1,36 +1,29 @@
 package dev.tokoi.notekeeperkt
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
-import dev.tokoi.notekeeperkt.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener { view ->
+            val originalValue = textDisplayedValue.text.toString().toInt()
+            val newValue = originalValue * 2
+            textDisplayedValue.text = newValue.toString()
+            Snackbar.make(
+                view, "Value $originalValue chaned to $newValue",
+                Snackbar.LENGTH_LONG
+            ).show()
         }
     }
 
@@ -48,11 +41,5 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
