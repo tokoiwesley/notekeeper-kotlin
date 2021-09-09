ead1d8e4e5d9aac25854e1b79be1ef2210e5fee8
Wesley Tokoi <tokoiwesley@gmail.com>
2021-09-09 23:37:34 +0300

    Initialize the data manager class

--- app/src/main/java/dev/tokoi/notekeeperkt/DataManager.kt
+++ app/src/main/java/dev/tokoi/notekeeperkt/DataManager.kt
@@ -3,4 +3,22 @@ package dev.tokoi.notekeeperkt
 class DataManager {
     val courses = HashMap<String, CourseInfo>()
     val notes = ArrayList<NoteInfo>()
+
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses() {
+        var course = CourseInfo("course_indents", "Android Programming with Intents")
+        courses.set(course.courseId, course)
+
+        course =
+            CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("java_core", "Java Fundamentals: The Core Platform")
+    }
 }
