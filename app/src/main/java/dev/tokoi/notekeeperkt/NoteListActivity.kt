11b13faf902619614e0c03874e6cf6cded7cd3ef
Wesley Tokoi <tokoiwesley@gmail.com>
2021-09-11 23:58:36 +0300

    Add the note list activity

---
+++ app/src/main/java/dev/tokoi/notekeeperkt/NoteListActivity.kt
@@ -0,0 +1,21 @@
+package dev.tokoi.notekeeperkt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class NoteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+        setSupportActionBar(toolbar)
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
