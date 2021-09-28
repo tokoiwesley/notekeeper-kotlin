d07bbee739e188cd91ecfa8493d513f666be8293
Wesley Tokoi <tokoiwesley@gmail.com>
2021-09-28 22:11:12 +0300

    Resolve the cause of an unhandled exception

--- app/src/main/java/dev/tokoi/notekeeperkt/MainActivity.kt
+++ app/src/main/java/dev/tokoi/notekeeperkt/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
             displayNote()
         } else {
             createNewNote()
+            notePosition = DataManager.notes.lastIndex
         }
     }
 
