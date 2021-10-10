baf784e81e32788c4e068895cc43dd22b25de6cd
Wesley Tokoi <tokoiwesley@gmail.com>
2021-10-10 23:53:20 +0300

    Add an inconsistent test behaviour

--- app/src/test/java/dev/tokoi/notekeeperkt/DataManagerTest.kt
+++ app/src/test/java/dev/tokoi/notekeeperkt/DataManagerTest.kt
@@ -19,4 +19,23 @@ class DataManagerTest {
         assertEquals(noteTitle, note.title)
         assertEquals(noteText, note.text)
     }
+
+    @Test
+    fun findSimilarNotes() {
+        val course = DataManager.courses.get("android_async")!!
+        val noteTitle = "This is a test note"
+        val noteText1 = "This is the body of my test note"
+        val noteText2 = "This is the body of my second test note"
+
+        val index1 = DataManager.addNote(course, noteTitle, noteText1)
+        val index2 = DataManager.addNote(course, noteTitle, noteText2)
+
+        val note1 = DataManager.findNote(course, noteTitle, noteText1)
+        val foundIndex1 = DataManager.notes.indexOf(note1)
+        assertEquals(index1, foundIndex1)
+
+        val note2 = DataManager.findNote(course, noteTitle, noteText2)
+        val foundIndex2 = DataManager.notes.indexOf(note2)
+        assertEquals(index2, foundIndex2)
+    }
 }
