db211c53f4ec715db8108fbd81b94d39167c9668
Wesley Tokoi <tokoiwesley@gmail.com>
2021-10-14 05:06:34 +0300

    Verify app logic during UI interaction

--- app/src/androidTest/java/dev/tokoi/notekeeperkt/CreateNewNoteTest.kt
+++ app/src/androidTest/java/dev/tokoi/notekeeperkt/CreateNewNoteTest.kt
@@ -11,6 +11,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 
 @RunWith(AndroidJUnit4::class)
 class CreateNewNoteTest {
@@ -31,8 +32,13 @@ class CreateNewNoteTest {
         onData(allOf(instanceOf(CourseInfo::class.java), equalTo(course))).perform(click())
 
         onView(withId(R.id.textNoteTitle)).perform(typeText(noteTitle))
-        onView(withId(R.id.textNoteText)).perform(typeText(noteText))
+        onView(withId(R.id.textNoteText)).perform(typeText(noteText), closeSoftKeyboard())
 
         pressBack()
+
+        val newNote = DataManager.notes.last()
+        assertEquals(course, newNote.course)
+        assertEquals(noteTitle, newNote.title)
+        assertEquals(noteText, newNote.text)
     }
 }
