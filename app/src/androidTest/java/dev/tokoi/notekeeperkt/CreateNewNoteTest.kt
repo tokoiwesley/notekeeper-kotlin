302d476b003174be1a6c244c626de7c03c05f48c
Wesley Tokoi <tokoiwesley@gmail.com>
2021-10-14 04:13:54 +0300

    Implement the automated UI test

--- app/src/androidTest/java/dev/tokoi/notekeeperkt/CreateNewNoteTest.kt
+++ app/src/androidTest/java/dev/tokoi/notekeeperkt/CreateNewNoteTest.kt
@@ -1,12 +1,17 @@
 package dev.tokoi.notekeeperkt
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class CreateNewNoteTest {
+
+    @Rule @JvmField
+    val noteListActivity = ActivityTestRule(NoteListActivity::class.java)
 
     @Test
     fun createNewNote() {
