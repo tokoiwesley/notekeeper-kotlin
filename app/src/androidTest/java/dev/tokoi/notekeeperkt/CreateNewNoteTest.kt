9fbd40317043d31d9170887143062c54efd4262a
Wesley Tokoi <tokoiwesley@gmail.com>
2021-10-14 04:23:08 +0300

    Implement basic test interactions

--- app/src/androidTest/java/dev/tokoi/notekeeperkt/CreateNewNoteTest.kt
+++ app/src/androidTest/java/dev/tokoi/notekeeperkt/CreateNewNoteTest.kt
@@ -1,5 +1,9 @@
 package dev.tokoi.notekeeperkt
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import org.junit.Assert.*
@@ -10,11 +14,19 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class CreateNewNoteTest {
 
-    @Rule @JvmField
+    @Rule
+    @JvmField
     val noteListActivity = ActivityTestRule(NoteListActivity::class.java)
 
     @Test
     fun createNewNote() {
+        val noteTitle = "Test note title"
+        val noteText = "This is the note of our body text"
+
+        onView(withId(R.id.fab)).perform(click())
+
+        onView(withId(R.id.textNoteTitle)).perform(typeText(noteTitle))
+        onView(withId(R.id.textNoteText)).perform(typeText(noteText))
 
     }
 }
