595dfd1cd34ce9b14e96660c99e33496199207f1
Wesley Tokoi <tokoiwesley@gmail.com>
2021-10-14 04:53:38 +0300

    Implement spinner and back button interactions

--- app/src/androidTest/java/dev/tokoi/notekeeperkt/CreateNewNoteTest.kt
+++ app/src/androidTest/java/dev/tokoi/notekeeperkt/CreateNewNoteTest.kt
@@ -1,15 +1,16 @@
 package dev.tokoi.notekeeperkt
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import org.hamcrest.Matchers.*
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import androidx.test.espresso.Espresso.pressBack
 
 @RunWith(AndroidJUnit4::class)
 class CreateNewNoteTest {
@@ -20,13 +21,18 @@ class CreateNewNoteTest {
 
     @Test
     fun createNewNote() {
+        val course = DataManager.courses["android_async"]
         val noteTitle = "Test note title"
         val noteText = "This is the note of our body text"
 
         onView(withId(R.id.fab)).perform(click())
 
+        onView(withId(R.id.spinnerCourses)).perform(click())
+        onData(allOf(instanceOf(CourseInfo::class.java), equalTo(course))).perform(click())
+
         onView(withId(R.id.textNoteTitle)).perform(typeText(noteTitle))
         onView(withId(R.id.textNoteText)).perform(typeText(noteText))
 
+        pressBack()
     }
 }
