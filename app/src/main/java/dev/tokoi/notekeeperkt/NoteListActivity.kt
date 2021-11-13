aeaf14aca07c255817882c96e1dc2c1148a285b4
Wesley Tokoi <tokoiwesley@gmail.com>
2021-11-13 22:10:55 +0300

    Add a recyclerview to the app

--- app/src/main/java/dev/tokoi/notekeeperkt/NoteListActivity.kt
+++ app/src/main/java/dev/tokoi/notekeeperkt/NoteListActivity.kt
@@ -18,22 +18,9 @@ class NoteListActivity : AppCompatActivity() {
             val activityIntent = Intent(this, NoteActivity::class.java)
             startActivity(activityIntent)
         }
-
-        listNotes.adapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_list_item_1,
-            DataManager.notes
-        )
-
-        listNotes.setOnItemClickListener { parent, view, position, id ->
-            val activityIntent = Intent(this, NoteActivity::class.java)
-            activityIntent.putExtra(NOTE_POSITION, position)
-            startActivity(activityIntent)
-        }
     }
 
     override fun onResume() {
         super.onResume()
-        (listNotes.adapter as ArrayAdapter<NoteInfo>).notifyDataSetChanged()
     }
 }
