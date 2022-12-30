0d3523610cc8a3145bd0dfb8a1f369ad8610aa14
Wesley Tokoi <tokoiwesley@gmail.com>
2022-12-30 06:44:46 +0300

    Manage views within a recyclerview adapter

--- app/src/main/java/dev/tokoi/notekeeperkt/NoteRecyclerAdapter.kt
+++ app/src/main/java/dev/tokoi/notekeeperkt/NoteRecyclerAdapter.kt
@@ -4,18 +4,16 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class NoteRecyclerAdapter(private val context: Context) :
     RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>() {
     private val layoutInflater = LayoutInflater.from(context)
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val itemView = layoutInflater.inflate(R.layout.item_note_list, parent, false)
+        return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -24,5 +22,10 @@ class NoteRecyclerAdapter(private val context: Context) :
 
     override fun getItemCount(): Int {
         TODO("Not yet implemented")
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textCourse = itemView.findViewById<TextView?>(R.id.textCourse)
+        val textTitle = itemView.findViewById<TextView?>(R.id.textTitle)
     }
 }
